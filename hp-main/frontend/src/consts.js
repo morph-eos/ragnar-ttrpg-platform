78be40af32f46eefb14f6b932c5b02ae840e710f
Stefano Sciacovelli <code@morpheos.it>
2023-07-12 21:12:31 +0200

    Implementing routes & constants

--- hp-main/frontend/src/consts.js
+++ hp-main/frontend/src/consts.js
@@ -3,7 +3,7 @@ let backend;
 if (window.location.origin.includes('localhost')) {
     backend = 'http://localhost:4000';
 } else {
-    backend = 'https://heatpeakstudio.onrender.com';
+    backend = 'https://heatpeakstudio.com';
 }
 
 module.exports = Object.freeze({
