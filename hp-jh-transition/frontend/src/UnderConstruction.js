25f046c321e082971f47fa454f8d8e226386dba3
Stefano Sciacovelli <code@morpheos.it>
2023-07-12 20:59:35 +0200

    Some personalization and UnderConstruction

---
+++ hp-jh-transition/frontend/src/UnderConstruction.js
@@ -0,0 +1,7 @@
+function UnderConstruction() {
+    return (
+        <h1>Website under construction~ Check @heatpeakstudio on Instagram to get more informations!</h1>
+    )
+}
+
+export default UnderConstruction
