378afa4556cde525402c18a32624c76a2ca60e9b
Stefano Sciacovelli <code@morpheos.it>
2023-07-12 21:57:14 +0200

    perfectioning of UnderCostruction

--- hp-jh-transition/frontend/src/UnderConstruction.js
+++ hp-jh-transition/frontend/src/UnderConstruction.js
@@ -1,7 +1,25 @@
 function UnderConstruction() {
     return (
-        <h1>Website under construction~ Check @heatpeakstudio on Instagram to get more informations!</h1>
-    )
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                backgroundColor: 'black',
+            }}
+        >
+            <img
+                src="https://media2.giphy.com/media/hV1dkT2u1gqTUpKdKy/giphy.gif?cid=6c09b952nzs1x9zhws2aj1t84dp2j7rnxanrer4f3jxxd6p5&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g"
+                alt="Under Construction"
+                style={{ width: '50%', height: 'auto' }}
+            />
+            <h1 style={{ color: 'white' }}>
+                Website under construction~ Check @heatpeakstudio on Instagram to get more information!
+            </h1>
+        </div>
+    );
 }
 
-export default UnderConstruction
+export default UnderConstruction;
