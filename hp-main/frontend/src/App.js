28cd22e36cb1cfef2764f5c925f7568bd29e5e1c
Stefano Sciacovelli <code@morpheos.it>
2023-07-12 20:19:34 +0200

    deletion of useless parts App.js

--- hp-main/frontend/src/App.js
+++ hp-main/frontend/src/App.js
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
-import consts from './consts';
-
 function App() {
-    const [message, setMessage] = useState("");
-
-    // Fetching message from backend on mount
-    useEffect(() => {
-        fetch(consts.BACKEND)
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message));
-    }, []);
 
     return (
         <div className="App">
-            <h1>{message}</h1>
+            <h1>Tutturu! Funziona!</h1>
         </div>
     );
 }
