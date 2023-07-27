0ebfb278f468ea36c1d3e03c19083b463cf33741
Stefano Sciacovelli <code@morpheos.it>
2023-07-27 16:56:20 +0200

    Frontend BugFix

---
+++ hp-jh-transition/frontend/src/Main.jsx
@@ -0,0 +1,23 @@
+export default function Main() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '90vh',
+        backgroundColor: 'black',
+      }}
+    >
+      <img
+        src="https://media2.giphy.com/media/hV1dkT2u1gqTUpKdKy/giphy.gif?cid=6c09b952nzs1x9zhws2aj1t84dp2j7rnxanrer4f3jxxd6p5&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g"
+        alt="Under Construction"
+        style={{ width: 'auto', height: 'auto' }}
+      />
+      <h1 style={{ color: 'white' }}>
+        Website under construction~ Check @heatpeakstudio on Instagram to get more informations!
+      </h1>
+    </div>
+  );
+};
