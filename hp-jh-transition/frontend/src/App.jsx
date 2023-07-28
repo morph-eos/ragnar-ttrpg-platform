abffaba413cb6c360992b7da90cb143e19c23d93
Stefano Sciacovelli <code@morpheos.it>
2023-07-28 18:37:17 +0200

    First attempt on Item and creation of NotFoundPage

--- hp-jh-transition/frontend/src/App.jsx
+++ hp-jh-transition/frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Main from './Main';
+import NotFoundPage from './NotFoundPage';
 import Rpg from './routes/Rpg';
 
 export default function App() {
   return (
     <div className="App">
       <div id="navbar">
-        <img src="/logo.png" alt="Logo"/>
+        <Link to="/">
+          <img src="/logo.png" alt="Logo" />
+        </Link>
         {/* Aggiungi altri elementi della navbar, come pulsanti o menu, a destra del logo */}
       </div>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/rpg" element={<Rpg />} />
-        <Route path="/rpg/:button" element={<Rpg />} />
-        <Route path=":default" element={<Main />} />
+        <Route path="/rpg/:button/*" element={<Rpg />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
