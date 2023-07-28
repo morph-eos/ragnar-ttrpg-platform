a4dcb960714368b9ab15266fff32b2acb69ce5bf
Stefano Sciacovelli <code@morpheos.it>
2023-07-28 17:17:21 +0200

    Set up Tailwind and start Rpg component

--- frontend/src/routes/Rpg.jsx
+++ frontend/src/routes/Rpg.jsx
@@ -1,3 +1,59 @@
-export default function Router() {
+import { Routes, Route, Link, useParams } from 'react-router-dom';
+import Sheets from './rpg/Sheets';
+import Classes from './rpg/Classes';
+import Items from './rpg/Items';
+import Races from './rpg/Races';
+import World from './rpg/World';
+
+export default function Rpg() {
+  const { button } = useParams();
   
-};
+  return (
+      <>
+        {/* Navbar */}
+        <nav className='p-4'>
+          <ul className='flex flex-wrap space-x-4 justify-center'>
+            <li className='flex-shrink-0'>
+              <Link
+                className={`btn-bullet btn-bullet-${button === 'sheets' ? 'sunset' : 'orange'}`}
+                to="/rpg/sheets"
+              >Schede</Link>
+            </li>
+            <li className='flex-shrink-0'>
+              <Link
+                className={`btn-bullet btn-bullet-${button === 'classes' ? 'sunset' : 'orange'}`}
+                to="/rpg/classes"
+              >Classi</Link>
+            </li>
+            <li className='flex-shrink-0'>
+              <Link
+                className={`btn-bullet btn-bullet-${button === 'items' ? 'sunset' : 'orange'}`}
+                to="/rpg/items"
+              >Oggetti</Link>
+            </li>
+            <li className='flex-shrink-0'>
+              <Link
+                className={`btn-bullet btn-bullet-${button === 'races' ? 'sunset' : 'orange'}`}
+                to="/rpg/races"
+              >Razze</Link>
+            </li>
+            <li className='flex-shrink-0'>
+              <Link
+                className={`btn-bullet btn-bullet-${button === 'world' ? 'sunset' : 'orange'}`}
+                to="/rpg/world"
+              >Mondo</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Route components */}
+        <Routes>
+          <Route path="/sheets" element={<Sheets />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/items" element={<Items/>}/>
+          <Route path="/races" element={<Races />} />
+          <Route path="/world" element={<World />} />
+        </Routes>
+      </>
+  );
+}
