228bde24100e2ebf2aa94bf4a5630437bab6f9b1
Stefano Sciacovelli <code@morpheos.it>
2023-08-13 13:57:35 +0200

    First Implementation of Ability List

--- hp-jh-transition/frontend/src/routes/rpg/functions.js
+++ hp-jh-transition/frontend/src/routes/rpg/functions.js
@@ -2,3 +2,14 @@ export function modifier(stat) {
   const mod = Math.floor((stat - 10) / 2);
   return mod >= 0 ? `+${mod}` : mod.toString();
 }
+
+export function findAbility(pathsArray, abilityName) {
+  for (const path of pathsArray) {
+    for (const ability of path.abilities) {
+      if (ability.name === abilityName) {
+        return ability;
+      }
+    }
+  }
+  return null;
+}
