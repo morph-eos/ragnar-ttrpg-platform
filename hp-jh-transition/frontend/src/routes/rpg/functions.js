66de7c2c5bb4791841495c23672f1f900b4106f3
Stefano Sciacovelli <code@morpheos.it>
2023-09-11 18:25:59 +0200

    sheet page and reference system bugfixed

--- hp-jh-transition/frontend/src/routes/rpg/functions.js
+++ hp-jh-transition/frontend/src/routes/rpg/functions.js
@@ -1,8 +1,3 @@
-export function modifier(stat) {
-  const mod = Math.floor((stat - 10) / 2);
-  return mod >= 0 ? `+${mod}` : mod.toString();
-}
-
 export function findAbility(pathsArray, abilityName) {
   for (const path of pathsArray) {
     for (const ability of path.abilities) {
