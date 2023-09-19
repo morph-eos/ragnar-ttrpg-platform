59e825e245bffb0066d07e408d5efde3ae649362
Stefano Sciacovelli <code@morpheos.it>
2023-09-19 20:42:21 +0200

    bugfixes, infrastructure upgrade and races page now fully implemented

--- frontend/src/routes/rpg/Showcase.jsx
+++ frontend/src/routes/rpg/Showcase.jsx
@@ -53,12 +53,12 @@ export default function Showcase({ type }) {
         <div className="w-1/3">
           {selectedOption && (
             <div className="bg-white rounded-lg shadow-lg p-4 max-h-[78vh] overflow-auto">
-              {selectedOption.description.references && (
+              {selectedOption.references && (
                 <div className="rounded-xl overflow-y-auto">
-                  {selectedOption.description.references.map((image, index) => (
+                  {selectedOption.references.map((image, index) => (
                     <div key={"reference_" + index} className="mb-4">
                       <img
-                        src={`${window.origin}/api/rpg/references/${style}_${selectedOption._id}_${image}`}
+                        src={`${window.origin}/api/rpg/references/${type}_${selectedOptionId}_${image}`}
                         alt={`Reference ${index + 1}`}
                         className="max-w-full"
                       />
@@ -76,24 +76,111 @@ export default function Showcase({ type }) {
                 {selectedOption.name}
               </h1>
               <p className="text-gray-600">{selectedOption.description}</p>
-              {(type === 'regions') && (
+              {type === "regions" && (
                 <div className="mt-4">
-                  <h2 className="text-xl font-semibold mb-2">Razze provenienti da qui</h2>
-                  <p>
-                    {selectedOption.races && selectedOption.races.map((race, index) => (
-                      <span key={race._id}>
-                        {race.name}
-                        {index < selectedOption.races.length - 1 && ", "}
-                      </span>
-                    ))}
-                    </p>
+                  {selectedOption.races && selectedOption.races.lenght > 0 && (
+                    <>
+                      <h2 className="text-xl font-semibold mb-2">
+                        Origine delle seguenti razze:
+                      </h2>
+                      <p>
+                        {selectedOption.races.map((race, index) => (
+                          <span key={race._id}>
+                            {race.name}
+                            {index < selectedOption.races.length - 1 && ", "}
+                          </span>
+                        ))}
+                      </p>
+                    </>
+                  )}
                   {selectedOption.languages && (
                     <>
-                      <h2 className="text-xl font-semibold mb-2 mt-4">Lingue parlate</h2>
+                      <h2 className="text-xl font-semibold mb-2 mt-4">
+                        Lingue parlate:
+                      </h2>
                       <p>{selectedOption.languages}</p>
                     </>
                   )}
                 </div>
+              )}
+              {type === "races" && selectedOption.statistics && (
+                <>
+                  <div className="mt-4">
+                    <h2 className="text-xl font-semibold mb-2">
+                      Statistiche iniziali
+                    </h2>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div>
+                        <p>
+                          <span className="font-bold text-orange-500">
+                            Costituzione:
+                          </span>{" "}
+                          {selectedOption.statistics.constitution}
+                        </p>
+                        <p>
+                          <span className="font-bold text-orange-500">
+                            Forza:
+                          </span>{" "}
+                          {selectedOption.statistics.strength}
+                        </p>
+                        <p>
+                          <span className="font-bold text-orange-500">
+                            Destrezza:
+                          </span>{" "}
+                          {selectedOption.statistics.dexterity}
+                        </p>
+                      </div>
+                      <div>
+                        <p>
+                          <span className="font-bold text-orange-500">
+                            Intelligenza:
+                          </span>{" "}
+                          {selectedOption.statistics.intelligence}
+                        </p>
+                        <p>
+                          <span className="font-bold text-orange-500">
+                            Saggezza:
+                          </span>{" "}
+                          {selectedOption.statistics.wisdom}
+                        </p>
+                        <p>
+                          <span className="font-bold text-orange-500">
+                            Carisma:
+                          </span>{" "}
+                          {selectedOption.statistics.charisma}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  {selectedOption.abilities.items[0] && (
+                    <div className="mt-4">
+                      <h2 className="text-xl font-semibold">
+                        Lista abilità di razza
+                      </h2>
+                      <div>
+                        {selectedOption.abilities.items.map(
+                          (ability, index) => (
+                            <div key={`Ability ${index}`} className="mt-2">
+                              <p className="font-bold text-orange-500">
+                                {ability.name}
+                              </p>
+                              <p>
+                                <span className="font-bold">
+                                  Ottenibile al livello:
+                                </span>{" "}
+                                {selectedOption.abilities.lvlsToGet[index]}
+                              </p>
+                              <p className="text-gray-600">
+                                <span className="font-bold">Descrizione:</span>{" "}
+                                {ability.description}
+                              </p>
+                            </div>
+                          ),
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </>
               )}
             </div>
           )}
