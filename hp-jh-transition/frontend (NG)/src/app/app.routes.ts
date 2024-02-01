deaeb4aedbfad6d5466cf45e1c59b1aa38dfa0f0
Stefano Sciacovelli <code@morpheos.it>
2024-02-01 18:03:17 +0100

    Bugfixes and new TTRPG Component

--- hp-jh-transition/frontend (NG)/src/app/app.routes.ts
+++ hp-jh-transition/frontend (NG)/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
+import { TTRPGComponent } from './pages/ttrpg/ttrpg.component';
 
-export const routes: Routes = [
+export const routes: Routes = [ 
+    { path: 'ttrpg', component: TTRPGComponent },
     { path: 'login', component: LoginComponent },
     { path: '', component: MainComponent }
 ];
