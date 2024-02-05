ce70c7ef9b6ea0795cf888971caca672d7a28604
Stefano Sciacovelli <code@morpheos.it>
2024-02-05 19:31:19 +0100

    Transition to Anular's non-strict mode

--- frontend/src/app/pages/ttrpg/ttrpg.component.ts
+++ frontend/src/app/pages/ttrpg/ttrpg.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ttrpg',
-  standalone: true,
-  imports: [],
   templateUrl: './ttrpg.component.html',
   styleUrl: './ttrpg.component.css'
 })
