141e10ba851b50128b85f6fd2261e17b87a40ece
Stefano Sciacovelli <code@morpheos.it>
2024-02-06 13:57:32 +0100

    Redirect every non-api route to the Angular app

---
+++ backend/src/main/java/com/heatpeakstudio/backend/config/MvcConfiguration.java
@@ -0,0 +1,33 @@
+package com.heatpeakstudio.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+ 
+import java.io.IOException;
+ 
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        if (resourcePath.startsWith("/api")) {
+                            // Return null to indicate that the resource does not exist
+                            return null;
+                        }
+                        Resource requestedResource = location.createRelative(resourcePath);
+ 
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+}
