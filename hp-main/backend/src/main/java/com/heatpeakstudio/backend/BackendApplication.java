589e0fdc783ba874a7b3d0361c8c28de78fce686
Stefano Sciacovelli <code@morpheos.it>
2024-02-06 13:35:19 +0100

    Revert "Redirect every non-api route to the Angular app"
    This reverts commit 434fcdbf67cebca9046d29ab1189e6dea37cf509.

--- hp-main/backend/src/main/java/com/heatpeakstudio/backend/BackendApplication.java
+++ hp-main/backend/src/main/java/com/heatpeakstudio/backend/BackendApplication.java
@@ -2,27 +2,12 @@ package com.heatpeakstudio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/**")
-                        .addResourceLocations("classpath:/static/")
-                        .resourceChain(false)
-                        .addResolver(new AngularPathResourceResolver());
-            }
-        };
-    }
 }
