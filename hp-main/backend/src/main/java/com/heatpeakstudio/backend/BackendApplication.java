21f81e340f8ba67e4a56e35e492702d2e3dd9787
Stefano Sciacovelli <code@morpheos.it>
2024-02-06 13:50:55 +0100

    Revert "Redirect every non-api route to the Angular app"
    This reverts commit 43eea5d4d2abd3040f6c8f72c868c9f7bc998e03.

--- hp-main/backend/src/main/java/com/heatpeakstudio/backend/BackendApplication.java
+++ hp-main/backend/src/main/java/com/heatpeakstudio/backend/BackendApplication.java
@@ -2,28 +2,12 @@ package com.heatpeakstudio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
 
-    @Configuration
-    public class WebConfig implements WebMvcConfigurer {
-
-        @Override
-        public void addViewControllers(ViewControllerRegistry registry) {
-            registry.addViewController("/{spring:\\w+}")
-                    .setViewName("forward:/");
-            registry.addViewController("/**/{spring:\\w+}")
-                    .setViewName("forward:/");
-            registry.addViewController("/{spring:^(?!api$).*$}/**")
-                    .setViewName("forward:/");
-        }
-    }
 }
