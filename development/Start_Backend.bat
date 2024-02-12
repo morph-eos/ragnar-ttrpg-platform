81e25af1314f44c79e13c331e08ec9165cd80098
Stefano Sciacovelli <code@morpheos.it>
2024-02-12 23:02:54 +0100

    Development tools ready

--- development/Start_Backend.bat
+++ development/Start_Backend.bat
@@ -4,21 +4,12 @@ REM Starting Docker Desktop
 echo Starting Docker Desktop...
 start /min "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
-REM Wait for Docker Desktop to start
-:WAIT_FOR_DESKTOP
-timeout /t 5 >nul
-tasklist /FI "IMAGENAME eq Docker Desktop.exe" | find /i "Docker Desktop.exe" >nul
-if not errorlevel 1 goto WAIT_FOR_ENGINE
-
-REM Wait for Docker Engine to start
-:WAIT_FOR_ENGINE
-timeout /t 5 >nul
-sc query docker | find "STATE" | find "RUNNING" >nul
-if %errorlevel% neq 0 goto STARTNOW
-
-:STARTNOW
 REM Remove existing container with the same name if exists
-docker rm -f db >nul 2>&1
+:STARTCHECK
+timeout 3
+docker rm -f HPS-PostgreSQL >nul 2>&1 || (
+	goto STARTCHECK
+)
 
 REM Run Docker container once Docker Engine is ready
 echo Starting Database...
@@ -27,13 +18,13 @@ docker run -d ^
   -p 5432:5432 ^
   -e POSTGRES_USER=hps ^
   -e POSTGRES_PASSWORD=example ^
-  -v %cd%/db:/var/lib/postgresql/data ^
   postgres:16
 
-echo Starting actual Backend...
+echo Building the actual Backend...
 REM Navigate to the backend directory
 cd ../backend/
 
 REM Build and run the Spring Boot application
-mvn clean package
+call mvn clean install
+echo Starting the actual Backend...
 java -jar target/backend.jar
