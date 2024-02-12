41c98a9524215fde401425b8c876deee8a0d2d17
Stefano Sciacovelli <code@morpheos.it>
2024-02-12 23:29:23 +0100

    Bugfixes regarding database connection
    In both development environments and on deployment server itself

--- development/Start_Backend.bat
+++ development/Start_Backend.bat
@@ -1,30 +1,10 @@
 @echo off
 
-REM Starting Docker Desktop
-echo Starting Docker Desktop...
-start /min "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-
-REM Remove existing container with the same name if exists
-:STARTCHECK
-timeout 3
-docker rm -f HPS-PostgreSQL >nul 2>&1 || (
-	goto STARTCHECK
-)
-
-REM Run Docker container once Docker Engine is ready
-echo Starting Database...
-docker run -d ^
-  --name HPS-PostgreSQL ^
-  -p 5432:5432 ^
-  -e POSTGRES_USER=hps ^
-  -e POSTGRES_PASSWORD=example ^
-  postgres:16
-
-echo Building the actual Backend...
 REM Navigate to the backend directory
 cd ../backend/
 
 REM Build and run the Spring Boot application
+echo Building the backend...
 call mvn clean install
-echo Starting the actual Backend...
+echo Starting the backend...
 java -jar target/backend.jar
