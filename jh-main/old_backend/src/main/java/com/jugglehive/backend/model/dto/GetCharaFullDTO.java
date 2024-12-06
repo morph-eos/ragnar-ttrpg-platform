38f187409dbd8a35fd7a3af81ba401031602d536
Stefano Sciacovelli <code@morpheos.it>
2024-12-06 13:45:43 +0100

    Old backend deleted

--- jh-main/old_backend/src/main/java/com/jugglehive/backend/model/dto/GetCharaFullDTO.java
+++
@@ -1,5 +0,0 @@
-package com.jugglehive.backend.model.dto;
-
-public class GetCharaFullDTO {
-
-}
--- jh-main/old_backend/src/main/java/com/jugglehive/backend/repository/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jugglehive.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jugglehive.backend.model.entity.ttrpg.Skill;
-
-public interface SkillRepository extends JpaRepository<Skill, Long>{
-
-}
